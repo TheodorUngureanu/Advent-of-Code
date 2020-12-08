5db72f3058628e36626ccf64f7e4f5c46c271757
Theodor Ungureanu <ungureanu.theo@gmail.com>
2020-12-08 12:08:54 +0200

    added day8

---
+++ 2020/day9/day9.py
@@ -0,0 +1,14 @@
+#day 9
+
+
+if __name__ == "__main__":
+	with open("input", 'r') as input:
+		lines = input.read().splitlines()
+		# print(lines)
+	
+	
+	print("Part1: " + str(max(seats)))
+
+	
+			print("Part2: " + str(rightSeat))
+			break
