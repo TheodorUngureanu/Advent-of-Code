0b982404dbd3305e371f7ad8f4fa4b165ccc3c7c
Theodor Ungureanu <ungureanu.theo@gmail.com>
2020-12-09 09:46:43 +0200

    added day9

--- 2020/day9/day9.py
+++ 2020/day9/day9.py
@@ -1,14 +1,52 @@
 #day 9
 
+def check(numbers, sumNumber):
+	for i in range(0, len(numbers) - 1):
+		for j in range(i+1, len(numbers)):
+			if numbers[i] + numbers[j] == sumNumber:
+				return True
+	return False
+
+
+def computePart1(numbers, previousMagicNumber):
+	number = None
+	for i in range(previousMagicNumber, len(numbers)):
+		if not check(numbers[i - previousMagicNumber : i], numbers[i]):
+			number = numbers[i]
+			break
+	return number
+
+
+def computePart2(numbers, part1):
+	for i in range(len(numbers)): 
+		currentSum = numbers[i] 
+
+		# finding subArrays
+		j = i + 1
+		while j <= len(numbers): 
+			if currentSum == part1: 
+				# the subArray sum is between i and j 
+				return max(numbers[i:j-1]), min(numbers[i:j-1]), max(numbers[i:j-1]) + min(numbers[i:j-1])
+						
+			if currentSum > part1 or j == len(numbers): 
+				break
+
+			# update current sum	
+			currentSum = currentSum + numbers[j] 
+			j += 1
+
+	return None, None, None
+
 
 if __name__ == "__main__":
 	with open("input", 'r') as input:
 		lines = input.read().splitlines()
 		# print(lines)
 	
-	
-	print("Part1: " + str(max(seats)))
+	numbers = []
+	for line in lines:
+		numbers.append(int(line))
 
-	
-			print("Part2: " + str(rightSeat))
-			break
+	part1 = computePart1(numbers, 25)
+	print("Part1: " + str(part1))	
+	print("Part2: " + str(computePart2(numbers, part1)[2]))	
